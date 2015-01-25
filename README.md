ce3e280821d9a51bdeac15ac52033137529cc1fa
Dan Fockler <dfockler13@gmail.com>
2015-01-25 14:58:38 -0800

    updated readme, got div moving around

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 ##Netrunner Table
 Trying to make a Node based game table to play netrunner on.
+
+###How to install
+This project uses node so install that first, along with npm.
+
+After you have node and npm installed, then in the project root run `npm install` to install dependencies from package.json
+
+Change line 3 of `app.js` to reflect your current IP or `localhost`, and change it in line 20 of `index.html` as well
+
+###How to Run
+In the project root, run `node app.js` or `nodejs app.js` if you are on linux
--- app.js
+++ app.js
@@ -1,14 +1,18 @@
 var express = require('express')
 var app = express();
-var io = require('socket.io')(app.listen(8080));
+var io = require('socket.io')(app.listen(8080, '192.168.2.20'));
 
 app.get('/', function (req, res) {
-  res.sendfile(__dirname + '/index.html');
+  res.sendFile(__dirname + '/public/index.html');
 });
 
 io.on('connection', function (socket) {
   socket.emit('news', { hello: 'world' });
   socket.on('my other event', function (data) {
     console.log(data);
+  });
+
+  socket.on('dragging', function(data) {
+  	socket.broadcast.emit('dragging', data);
   });
 });
