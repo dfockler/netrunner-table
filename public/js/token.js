65ca471dfd40fe2f99aa4f362763ccaad1d311c3
Dan Fockler <dfockler13@gmail.com>
2015-01-26 20:55:34 -0800

    messing around with adding multiple objects, and tapping cards

--- public/js/token.js
+++ public/js/token.js
@@ -1,18 +1,21 @@
 //Used for a base token object
 require(["jquery", "jquery-ui", "socketio"], function($, ui, io){
-	$(document).ready(function(){
-		$('.dragme').draggable({
-			drag: function( event, ui ) {
-				socket.emit('dragging', { position: ui.position });
-			}
-		}).click(function(){
-			$(this).toggleClass("zoomed");
-		});
+	var socket = io.connect('http://192.168.2.20:8080');
+	$('.dragme').draggable({
+		drag: function( event, ui ) {
+			socket.emit('dragging', { position: ui.position, elem: $(this)[0].id });
+		}
+	}).click(function(){
+		$(this).toggleClass("zoomed");
+		socket.emit('clicked', { elem: $(this)[0].id });
+	});
 
-		var socket = io.connect('http://192.168.2.20:8080');
-		socket.on('dragging', function(data) {
-			console.log(data);
-			$('.dragme').css(data.position);
-		});
+	socket.on('dragging', function(data) {
+		console.log(data);
+		$("#"+data.elem).css(data.position);
+	});
+
+	socket.on('clicked', function(data) {
+		$("#"+data.elem).toggleClass("zoomed");
 	});
 });
